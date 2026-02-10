c60ce8bd6c6ba9d7714b4b789b2adde9c1a4163f
Devansh Karamchandani <devansh.kc96@gmail.com>
2026-02-10 23:05:44 +0530

    feat(db): setup convex db for  storing data

---
+++ app/features/auth/components/auth-loading-view.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "@/components/ui/spinner";
+import React from "react";
+
+function AuthLoadingView() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Spinner className="size-6 text-ring" />
+    </div>
+  );
+}
+
+export default AuthLoadingView;
